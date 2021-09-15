1f4ad5ebfa6539e087e74d03008aebdced9c3596
saattrupdan <saattrupdan@gmail.com>
2021-09-15 18:32:58 +0100

    feat: Try evaluating two datasets

--- scandeval/utils.py
+++ scandeval/utils.py
@@ -25,6 +25,34 @@ class DepTrainer(Trainer):
         loss = super().compute_loss(*args, **kwargs)
         return loss
 
+    def _maybe_log_save_evaluate(self, tr_loss, model, trial, epoch, ignore_keys_for_eval):
+        if self.control.should_log:
+            logs: Dict[str, float] = {}
+            tr_loss_scalar = tr_loss.item()
+            # reset tr_loss to zero
+            tr_loss -= tr_loss
+
+            logs["loss"] = round(tr_loss_scalar / (self.state.global_step - self._globalstep_last_logged), 4)
+            logs["learning_rate"] = self._get_learning_rate()
+
+            self._total_loss_scalar += tr_loss_scalar
+            self._globalstep_last_logged = self.state.global_step
+            self.store_flos()
+
+            self.log(logs)
+
+        metrics = None
+        if self.control.should_evaluate:
+            metrics = self.evaluate(ignore_keys=ignore_keys_for_eval)
+            self._report_to_hp_search(trial, epoch, metrics)
+            metrics = self.evaluate(ignore_keys=ignore_keys_for_eval,
+                                    eval_dataset=self.train_dataset)
+            self._report_to_hp_search(trial, epoch, metrics)
+
+        if self.control.should_save:
+            self._save_checkpoint(model, trial, metrics=metrics)
+            self.control = self.callback_handler.on_save(self.args, self.state, self.control)
+
     def prediction_step(self, *args, **kwargs):
         import torch
 
