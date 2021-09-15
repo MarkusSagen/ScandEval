461cce7ff2759848140a8e45823573384d772983
saattrupdan <saattrupdan@gmail.com>
2021-09-15 17:09:50 +0100

    feat: Dep hyperparams

--- scandeval/dependency_parsing/parser.py
+++ scandeval/dependency_parsing/parser.py
@@ -22,7 +22,7 @@ class AutoModelForDependencyParsing(AutoModelForTokenClassification):
                         num_deps: int = 37,
                         head_dim: int = 500,
                         dep_dim: int = 100,
-                        emb_dropout: float = 0.50,
+                        emb_dropout: float = 0.20,
                         head_dropout: float = 0.50,
                         dep_dropout: float = 0.50,
                         **kwargs):
@@ -126,21 +126,21 @@ class BiaffineDependencyParser(nn.Module):
                  dep_dropout: float):
         super().__init__()
         self.head_mlp_d = MLP(in_dim=hidden_dim,
-                             out_dim=head_dim,
+                             out_dim=hidden_dim,
                              dropout=head_dropout)
         self.head_mlp_h = MLP(in_dim=hidden_dim,
-                             out_dim=head_dim,
+                             out_dim=hidden_dim,
                              dropout=head_dropout)
         self.dep_mlp_d = MLP(in_dim=hidden_dim,
-                             out_dim=dep_dim,
+                             out_dim=hidden_dim,
                              dropout=dep_dropout)
         self.dep_mlp_h = MLP(in_dim=hidden_dim,
-                             out_dim=dep_dim,
+                             out_dim=hidden_dim,
                              dropout=dep_dropout)
-        self.head_attn = Biaffine(in_dim=head_dim,
+        self.head_attn = Biaffine(in_dim=hidden_dim,
                                  bias_x=True,
                                  bias_y=False)
-        self.dep_attn = Biaffine(in_dim=dep_dim,
+        self.dep_attn = Biaffine(in_dim=hidden_dim,
                                  out_dim=num_deps,
                                  bias_x=True,
                                  bias_y=True)
